7e8cb0f11f153afafdadde3e3197bcdfe65c5294
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-03-07 11:49:53 +0100

    feat(app): add socket cellClicked and updateCell

--- server/app.js
+++ server/app.js
@@ -4,68 +4,73 @@ const { selectColor } = require('./lib/colorAssign.js');
 const { generateMessage } = require('./lib/message.js');
 
 const io = require('socket.io')(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
+	cors: {
+		origin: '*',
+		methods: ['GET', 'POST'],
+	},
 });
 
 // List of all rooms
 const allRooms = [];
 
 io.on('connection', (socket) => {
-  console.log(`User connected: ${socket.id}`);
-  //console.log("connection", socket)
+	console.log(`User connected: ${socket.id}`);
+	//console.log("connection", socket)
 
-  //Send to ONE
-  socket.emit('chat', generateMessage('Admin', 'Welcome to Color Chaos!'));
+	//Send to ONE
+	socket.emit('chat', generateMessage('Admin', 'Welcome to Color Chaos!'));
 
-  //send to everyone but "me"
-  socket.broadcast.emit(
-    'newMessage',
-    generateMessage('Admin', `New user has joined`)
-  );
+	//send to everyone but "me"
+	socket.broadcast.emit(
+		'newMessage',
+		generateMessage('Admin', `New user has joined`)
+	);
 
-  socket.on('chat', (arg) => {
-    console.log('incoming chat', arg);
-    console.log('to room', arg.room);
-    console.log(`User ${socket.id} connected to rooms:`, socket.rooms);
-    io.to(arg.room).emit(
-      'chat',
-      generateMessage(arg.user, arg.message, arg.room, arg.color)
-    );
-  });
+	socket.on('chat', (arg) => {
+		console.log('incoming chat', arg);
+		console.log('to room', arg.room);
+		console.log(`User ${socket.id} connected to rooms:`, socket.rooms);
+		io.to(arg.room).emit(
+			'chat',
+			generateMessage(arg.user, arg.message, arg.room, arg.color)
+		);
+	});
 
-  // Send list of all rooms to every client
-  socket.on('get rooms', () => {
-    io.emit('room list', allRooms);
-  });
+	// Send list of all rooms to every client
+	socket.on('get rooms', () => {
+		io.emit('room list', allRooms);
+	});
 
-  // Add the new room to the allRooms array
-  socket.on('create room', (room) => {
-    allRooms.push(room);
-  });
+	// Add the new room to the allRooms array
+	socket.on('create room', (room) => {
+		allRooms.push(room);
+	});
 
-  // Allow the client to join specific room
-  socket.on('join room', (room) => {
-    const color = selectColor(room);
-    socket.join(room);
-    console.log('joined room:', room);
-    console.log(
-      `User ${socket.id} connected to rooms:`,
-      socket.rooms,
-      ' with color:',
-      color
-    );
-    socket.emit('room joined', {
-      room: room,
-      color: color,
-    });
-  });
+	// Allow the client to join specific room
+	socket.on('join room', (room) => {
+		const color = selectColor(room);
+		socket.join(room);
+		console.log('joined room:', room);
+		console.log(
+			`User ${socket.id} connected to rooms:`,
+			socket.rooms,
+			' with color:',
+			color
+		);
+		socket.emit('room joined', {
+			room: room,
+			color: color,
+		});
+	});
+	// Hantera när en spelare klickar på en cell
+	socket.on('cellClicked', ({ row, col }) => {
+	// Här kan du lägga till logik för att hantera vilken spelare som klickade och uppdatera alla andra klienter
+	io.emit('updateCell', { row, col, user});
+	});
 });
 
 app.get('/test', (req, res) => {
-  res.send('<h1>socket</h1>');
+	res.send('<h1>socket</h1>');
 });
 
 server.listen(3000);
