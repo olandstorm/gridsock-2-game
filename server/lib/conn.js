79797ea5f6a1f1650abe8c1a020b8831e8975139
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-03-07 20:19:23 +0100

    feat(LIB)conn.js: add connection to mysql database

---
+++ server/lib/conn.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DB
+});
+
+module.exports = connection;
