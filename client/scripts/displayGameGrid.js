3bd7173db5a5fe7dc9ab31bd60c1763d177524a4
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-03-07 11:52:43 +0100

    feat(displayGameGrid): add createGameGrid function to render grid and eventlisten on cells clicked

---
+++ client/scripts/displayGameGrid.js
@@ -0,0 +1,32 @@
+import { socket } from '../main.js';
+
+
+export default function createGameGrid(gridContainer) {
+    
+    //const gridContainer = document.getElementById('grid-container');
+
+    for (let x = 0; x < 15; x++) {
+        for (let y = 0; y < 15; y++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.dataset.row = x;
+            cell.dataset.col = y;
+            gridContainer.appendChild(cell);
+        }
+    }
+
+    gridContainer.addEventListener('click', (event) => {
+        const clickedCell = event.target;
+        const row = clickedCell.dataset.row;
+        const col = clickedCell.dataset.col;
+    
+        socket.emit('cellClicked', { row, col });
+    });
+    
+    socket.on('updateCell', ({ row, col, user}) => {
+        const cell = document.querySelector(`.cell[data-row="${row}"][data-col="${col}"]`);
+        cell.classList.add(`user${user}`);
+    });
+}
+
+
