369b2b39566b84b9ef075b1ca3e2dcd02650edbf
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-03-12 15:27:02 +0100

    feat(SERVER)calculateresult:

---
+++ server/lib/calculateResult.js
@@ -0,0 +1,18 @@
+ export default function calculateResult() {
+    const playerScores = { /* number of colored celss for each player */ };
+    
+    for (let row = 0; row < 25; row++) {
+      for (let col = 0; col < 25; col++) {
+        const cell = gameGrid[row][col];
+        if (cell && cell.color && cell.player) {
+          if (!playerScores[cell.player]) {
+            playerScores[cell.player] = 1;
+          } else {
+            playerScores[cell.player]++;
+          }
+        }
+      }
+    }
+  
+    return playerScores;
+  }
