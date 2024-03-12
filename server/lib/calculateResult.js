f5467425d9537efbf7a41b456bbdfbaedecd2c6d
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-03-12 18:58:55 +0100

    feat(calculateResult) in backend calculate game result and return playerScores to client. check how many cells of each color in grid.

--- server/lib/calculateResult.js
+++ server/lib/calculateResult.js
@@ -1,18 +1,21 @@
- export default function calculateResult() {
-    const playerScores = { /* number of colored celss for each player */ };
-    
-    for (let row = 0; row < 25; row++) {
-      for (let col = 0; col < 25; col++) {
-        const cell = gameGrid[row][col];
-        if (cell && cell.color && cell.player) {
-          if (!playerScores[cell.player]) {
-            playerScores[cell.player] = 1;
-          } else {
-            playerScores[cell.player]++;
-          }
-        }
-      }
-    }
-  
-    return playerScores;
-  }
+function calculateResult(gameGrid) {
+	const playerScores = {
+		/* number of colored celss for each player */
+	};
+
+	for (let row = 0; row < 25; row++) {
+		for (let col = 0; col < 25; col++) {
+			const cell = gameGrid[row][col];
+			if (cell && cell.color && cell.player) {
+				if (!playerScores[cell.player]) {
+					playerScores[cell.player] = 1;
+				} else {
+					playerScores[cell.player]++;
+				}
+			}
+		}
+	}
+	return playerScores;
+}
+
+module.exports = calculateResult;
