70f7cdce8894a61d6de5f0aff058335e8e648e59
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-03-14 11:35:22 +0100

    :pencil2: typeo(old code): removes old out-commented code

--- client/main.js
+++ client/main.js
@@ -4,7 +4,7 @@ import printStart from './scripts/displayStartPage.js';
 import displayMainPage from './scripts/displayMainPage';
 
 export const API_URL =
-  'https://lionfish-app-dp3is.ondigitalocean.app/' || 'http://localhost:3000/';
+  /* 'https://lionfish-app-dp3is.ondigitalocean.app/' || */ 'http://localhost:3000/';
 export const socket = io(API_URL);
 
 // User state check
@@ -13,28 +13,3 @@ if (localStorage.getItem('user')) {
 } else {
   printStart();
 }
-
-/* let sendMessage = document.querySelector('#sendMessage');
-let sendBtn = document.querySelector('#sendBtn');
-let chatList = document.querySelector('#chatList');
-
-sendBtn.addEventListener('click', () => {
-  let user = sessionStorage.getItem('user');
-  console.log('send chat', sendMessage.value);
-  socket.emit('chat', {
-    message: sendMessage.value,
-    user: user,
-  });
-});
-
-socket.on('chat', (arg) => {
-  console.log('socket', arg);
-
-  updateChat(arg);
-});
-
-function updateChat(chat) {
-  let li = document.createElement('li');
-  li.innerText = chat.user + ': ' + chat.message;
-  chatList.appendChild(li);
-} */
